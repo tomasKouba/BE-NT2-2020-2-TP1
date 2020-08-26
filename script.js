98ad1581fe9d57f7bbd7b45646288a704bb96913
TOMAS AUGUSTO KOUBA <36191494@comunidadort.edu.ar>
2020-08-25 22:25:34 -0300

    master_default

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
 const classNames = {
   TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
 }
+
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function addTodo() {
-  
-  let item = document.createElement('li');
-  let item2 = document.createElement("input");
-  uncheckedCountSpan.innerText++;
-  itemCountSpan.innerText++;
-  //let contador = itemCountSpan;
-  item.textContent = "Nueva Tarea";
-  item.className = 'todo-text';
-  item2.type = "checkbox";
-  item2.className = classNames.TODO_CHECKBOX;
-  item2.onclick = function () {
-    if (item2.checked){      
-      uncheckedCountSpan.innerText--;
-    } else if (!item2.checked) {      
-      uncheckedCountSpan.innerText++;
-    }
-  };
-  list.append(item);
-  item.append(item2);
-  
+  alert('Boton Add TODO clickeado!')
 }
-
-
